cc7dec5d7498bb1ef51df9f4837dad722ef40429
Kimpaliz <83219000+Kimpaliz@users.noreply.github.com>
2025-08-02 00:57:54 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import "../globals.css";
+import "../styles/globals.css";
+
 import type { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
@@ -8,4 +9,3 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     </html>
   );
 }
-
